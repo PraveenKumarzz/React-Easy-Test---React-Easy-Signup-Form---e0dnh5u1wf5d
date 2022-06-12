8156c21f6fd5a02480361764cc6b5f870f301fa8
Praveen Kumar <101964246+PraveenKumarzz@users.noreply.github.com>
2022-06-12 22:48:25 +0530

    4th commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,129 +1,54 @@
 import React, { useState } from "react";
 import "../styles/App.css";
+import signUpFormValidation from "../utils/validation";
 
-export default function Form() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [checked, setChecked] = useState(false);
+const App = () => {
+  const formdata = {
+    name: "",
+    email: "",
+    password: ""
+  };
+  const [data, setData] = useState(formdata);
 
-  // States for checking the errors
-  const [submitted, setSubmitted] = useState(false);
-  const [error, setError] = useState(false);
-
-  // Handling the name change
-  const handleName = (e) => {
-    setName(e.target.value);
-    setSubmitted(false);
+  const changeName = (e) => {
+    setData({
+      name: e.target.value
+    });
   };
 
-  // Handling the email change
-  const handleEmail = (e) => {
-    setEmail(e.target.value);
-    setSubmitted(false);
+  const changeEmail = (e) => {
+    setData({
+      email: e.target.value
+    });
   };
-
-  // Handling the password change
-  const handlePassword = (e) => {
-    setPassword(e.target.value);
-    setSubmitted(false);
-  };
-
-  // Handling the form submission
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (name === "" || email === "" || password === "") {
-      setError(true);
-    } else {
-      setSubmitted(true);
-      setError(false);
-    }
-  };
-
-  // Showing success message
-  const successMessage = () => {
-    return (
-      <div
-        className="success"
-        style={{
-          display: submitted ? "" : "none"
-        }}
-      >
-        <h1>User {name} successfully registered!!</h1>
-      </div>
-    );
-  };
-
-  // Showing error message if error is true
-  const errorMessage = () => {
-    return (
-      <div
-        className="error"
-        style={{
-          display: error ? "" : "none"
-        }}
-      >
-        <h1>Please enter all the fields</h1>
-      </div>
-    );
-  };
-
-  // Handling the check
-
-  const handleCheck = (e) => {
-    setPassword(e.target.value);
-    setChecked(true);
+  const changePassword = (e) => {
+    setData({
+      password: e.target.value
+    });
   };
 
   return (
-    <div className="form">
-      {/* Calling to the methods */}
-      <div className="messages">
-        {errorMessage()}
-        {successMessage()}
-      </div>
+    <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          signUpFormValidation(data);
+        }}
+      >
+        <label>Name</label>
+        <input type="text" id="name" onChange={changeName} value={data.name} />
 
-      <form>
-        {/* Labels and inputs for form data */}
-        <label className="label">Name</label>
-        <input
-          onChange={handleName}
-          className="input"
-          id="name"
-          value={name}
-          type="text"
-        />
-
-        <label className="label">Email</label>
-        <input
-          id="email"
-          onChange={handleEmail}
-          className="input"
-          value={email}
-          type="email"
-        />
-
-        <label className="label">Password</label>
-        <input
-          id="password"
-          onChange={handlePassword}
-          className="input"
-          value={password}
-          type="password"
-        />
-        <label className="label">
-          <input
-            type="checkbox"
-            id="consent"
-            defaultChecked={checked}
-            onChange={handleCheck}
-          />
-        </label>
-
-        <button onClick={handleSubmit} className="btn" type="submit">
-          Signup
-        </button>
+        <label>email</label>
+        <input type="email" id="email" onChange={changeEmail} />
+        <label>password</label>
+        <input type="password" id="password" onChange={changePassword} />
+        <label>checkbox</label>
+        <input type="checkbox" id="consent" />
+        <label>Sign up</label>
+        <input type="submit" value="Signup" />
       </form>
-    </div>
+    </>
   );
-}
+};
+
+export default App;
